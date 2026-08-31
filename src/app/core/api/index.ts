f303a8b8ffc9978a48d2211299257d2a3d0e47f7
opanlabsjobs <opanlabsjobs@gmail.com>
2026-08-31 18:13:55 +0700

    fix(analyze): add decimals for pie chart

--- src/app/core/api/index.ts
+++ src/app/core/api/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://api-dev.skema.co.id/api';
+export const BASE_URL = 'https://api.skema.co.id/api';
 export const INFLUENCER_BASE_URL = 'https://api-influencer.skema.co.id/api';
 export const GEO_BASE_URL = 'https://api-geo.skema.co.id/api';
